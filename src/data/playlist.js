c4515429d7ebed7221431a01ded89886068e2452
Parambrata Mitra <parambratamitra@Mac.lan>
2026-08-17 23:28:48 +0530

    dildara

--- src/data/playlist.js
+++ src/data/playlist.js
@@ -27,7 +27,7 @@ export const playlist = [
     id: 'dildaara',
     title: 'Dildaara',
     artist: 'Shafqat Amanat Ali, Vishal Dadlani, Shekhar Ravjiani, Clinton Cerejo',
-    youtubeId: 'p9aW105LmEA',
+    youtubeId: 'gQ5qVtoLMk4',
   },
 
   {
